327511f4b28e19f3576709d0440f76262f7b4e5a
TANAWIT <tanawit.jaru@bumail.net>
2022-02-23 18:31:49 +0700

    Add "Logo" icon, "Home" webpage and "Friend.js"

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div>
+            This is "Home" page enter "/app","/profile" or "/minigame" on website to change page.
+        </div>
+    )
+}
+
+export default Home;
--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,5 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './Home'
 import App from './App'
 import Profile from './Profile';
 import MiniGame from './MiniGame';
@@ -8,6 +9,7 @@ function Router()
     return(
        <BrowserRouter>
        <Routes>
+           <Route path="/" element = {<Home/>}/>
            <Route path="/app" element = {<App/>}/>
            <Route path="/profile" element = {<Profile/>}/>
            <Route path="/minigame" element = {<MiniGame/>}/>
